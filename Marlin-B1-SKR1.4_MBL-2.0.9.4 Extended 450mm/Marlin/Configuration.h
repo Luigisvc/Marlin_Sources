0bb18130b6b054709a3cf575989b32d0fc57c25f
Luigisvc <lula.svc@gmail.com>
2022-06-20 17:55:41 -0300

    commit

--- Marlin-B1-SKR1.4_MBL-2.0.9.4 Extended 450mm/Marlin/Configuration.h
+++ Marlin-B1-SKR1.4_MBL-2.0.9.4 Extended 450mm/Marlin/Configuration.h
@@ -1465,8 +1465,8 @@
 #define X_MIN_POS 0
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS Y_BED_SIZE + 5
+#define X_MAX_POS X_BED_SIZE + 5
+#define Y_MAX_POS Y_BED_SIZE + 10
 #define Z_MAX_POS 450
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
